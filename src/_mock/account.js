8a7101268bc20292d3498046b4a9cf37c61e1357
Juan Prasetya <juanprasetyaaditama@smkwikrama.sch.id>
2023-08-18 10:48:21 +0700

    first commit

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Hexaon Bussines',
+  email: 'demo@minimals.cc',
+  photoURL: '/assets/images/avatars/HBM logo.png',
+};
+
+export default account;
